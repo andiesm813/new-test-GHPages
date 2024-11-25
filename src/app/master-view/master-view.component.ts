417036d5e50750c067fe5d7dcf75cb3af3c07554
andiesm813 <86068582+andiesm813@users.noreply.github.com>
2024-11-25 13:15:19 -0300

    new-test-GHPages initial version

---
+++ src/app/master-view/master-view.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { IGX_CARD_DIRECTIVES, IgxButtonDirective, IgxIconButtonDirective, IgxIconComponent, IgxOverlayOutletDirective, IgxRippleDirective, IgxToggleActionDirective, IgxToggleDirective } from 'igniteui-angular';
+import { Subject, takeUntil } from 'rxjs';
+import { BoxOfficeRevenueType } from '../models/financial/box-office-revenue-type';
+import { FinancialService } from '../services/financial.service';
+
+@Component({
+  selector: 'app-master-view',
+  standalone: true,
+  imports: [IGX_CARD_DIRECTIVES, IgxToggleActionDirective, IgxOverlayOutletDirective, IgxIconButtonDirective, IgxButtonDirective, IgxRippleDirective, IgxToggleDirective, IgxIconComponent],
+  templateUrl: './master-view.component.html',
+  styleUrls: ['./master-view.component.scss']
+})
+export class MasterViewComponent implements OnInit, OnDestroy {
+  private destroy$: Subject<void> = new Subject<void>();
+  public financialBoxOfficeRevenue: BoxOfficeRevenueType[] = [];
+
+  constructor(
+    private financialService: FinancialService,
+  ) {}
+
+  ngOnInit() {
+    this.financialService.getBoxOfficeRevenue().pipe(takeUntil(this.destroy$)).subscribe({
+      next: (data) => this.financialBoxOfficeRevenue = data,
+      error: (_err: any) => this.financialBoxOfficeRevenue = []
+    });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
